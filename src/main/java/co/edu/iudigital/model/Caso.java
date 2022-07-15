89cd7d8291b2baad9b4fb670708207f8a3cdf169
Dvn2014 <85833322+Dvn2014@users.noreply.github.com>
2022-07-14 20:42:15 -0500

    Creacion de los modelos

---
+++ src/main/java/co/edu/iudigital/model/Caso.java
@@ -0,0 +1,22 @@
+package co.edu.iudigital.model;
+
+public class Caso {
+	
+	
+	//id INT NOT NULL AUTO_INCREMENT,
+    //fecha_hora DATETIME NULL DEFAULT now(),
+    //latitud FLOAT NULL,
+    //longitud FLOAT NULL,
+    //altitud FLOAT NULL,
+    //visible TINYINT NULL DEFAULT 1,
+    //descripcion VARCHAR(200) NULL,
+    //url_map TEXT NULL,
+    //rmi_url TEXT NULL,
+    //usuarios_id INT NOT NULL,
+    //delitos_id INT NOT NULL,
+    //PRIMARY KEY(id),
+    //FOREIGN KEY (usuarios_id) REFERENCES usuarios(id),
+    //FOREIGN KEY (delitos_id) REFERENCES delitos(id)
+	
+
+}
